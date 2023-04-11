7bc2533475e3eda45f5a4e1705560f0ee1e52057
Tushar Kumar Verma <tushar.workplace@gmail.com>
2023-04-11 23:44:01 +0530

    Update ExpenseForm.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -22,7 +22,7 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: +enteredAmount,
+      amount: enteredAmount,
       date: new Date(enteredDate),
     };
 
