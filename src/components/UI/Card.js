c6d751414a54236566e7bb4fc02c9af846a0d65d
Tushar Kumar Verma <tushar.workplace@gmail.com>
2023-06-11 17:12:09 +0530

    Update Card.js

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,8 +1,7 @@
 import "./Card.css";
 
 const Card = (props) => {
-  const classes = "card " + props.className;
-  return <div className={classes}>{props.children}</div>;
+  return <div className={`card ${props.className}`}>{props.children}</div>;
 };
 
 export default Card;
